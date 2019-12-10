4cb673b72e45a415fe35df3ff8a53e5d202c927d
yuki3738 <y.minamiya@gmail.com>
2019-12-10 23:55:38 +0900

    Change abc147_b.rb

--- contests/abc147/tasks/abc147_b.rb
+++ contests/abc147/tasks/abc147_b.rb
@@ -1,13 +1,2 @@
-s = gets.chomp.split('')
-n = (s.count / 2.0).floor - 1
-result = []
-
-0.upto(n) do |i|
-  if s[i] == s[-(i + 1)]
-    next
-  else
-    result << i
-  end
-end
-
-puts result.count
+s = gets.chop.chars
+p s.count{ |x| x != s.pop }
